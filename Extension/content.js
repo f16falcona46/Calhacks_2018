ccd794a40e76134a43fe8738c22bbf3a53d261ae
Elden Li <lielden@yahoo.com>
2018-11-03 12:09:26 -0700

    put files into a folder

---
+++ Extension/content.js
@@ -0,0 +1,9 @@
+// alert("let's get this fucking bread");
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            console.log(request.incognitos);
+        } 
+    }
+)
