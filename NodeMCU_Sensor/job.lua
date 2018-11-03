368cf5719bd396b09311166d1490e19246fa5b4a
Jason Li <li07.jason@gmail.com>
2018-11-03 02:30:11 -0700

    Added Lua sensor code

---
+++ NodeMCU_Sensor/job.lua
@@ -0,0 +1,37 @@
+dofile("sensor.lua")
+last_state = nil
+function register_sensor()
+	http.get("https://jasonli.us/cgi-bin/webapp.cgi?action=add_sensor", "\r\n", function(code, body, headers)
+		print(code)
+		if code == 200 then
+			sensor_id = body
+			print(sensor_id)
+			tmr.create():alarm(200, tmr.ALARM_AUTO, function(timer)
+				if read_total_mag() > 6000000 then
+					print("CLOSED")
+					if last_state ~= 0 then
+						http.get("https://jasonli.us/cgi-bin/webapp.cgi?action=door_close&id="..sensor_id, "\r\n", function(code, body, headers)
+							print("CLOSED SET")
+							last_state = 0
+						end)
+					end
+				else
+					print("OPEN")
+					if last_state ~= 1 then
+						http.get("https://jasonli.us/cgi-bin/webapp.cgi?action=door_open&id="..sensor_id, "\r\n", function(code, body, headers)
+							print("OPEN SET")
+							last_state = 1
+						end)
+					end
+				end
+			end)
+		else
+			register_sensor()
+		end
+	end)
+end
+
+dofile("wifi_config.lua")
+wifi_config.got_ip_cb = register_sensor
+wifi.sta.config(wifi_config)
+wifi.sta.connect()
---
+++ NodeMCU_Sensor/wifi_config.lua
@@ -0,0 +1,7 @@
+wifi.setmode(wifi.STATION)
+
+wifi_config = {}
+wifi_config.ssid = "NodeMCU"
+wifi_config.pwd = "calhacks2018"
+wifi_config.save = true
+wifi_config.auto = true
