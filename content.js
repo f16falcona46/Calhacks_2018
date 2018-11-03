9b3fc7e9144df16ec038835bb67f027a16d0c9d5
Elden Li <lielden@yahoo.com>
2018-11-03 12:14:47 -0700

    cleaned things up

--- content.js
+++
@@ -1,9 +0,0 @@
-// alert("let's get this fucking bread");
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.message === "clicked_browser_action") {
-            console.log(request.incognitos);
-        } 
-    }
-)
